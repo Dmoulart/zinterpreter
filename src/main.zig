4a474518887f03839d6105c11d36912725bf8bc2
Dorian <dorian@resalocal.com>
2023-08-19 12:00:03 +0200

    don't exit the repl when parse err is raised

--- src/main.zig
+++ src/main.zig
@@ -63,17 +63,17 @@ fn run(src: []const u8) !void {
 
     if (parser.parse()) |ast| {
         // var buffer: [1024]u8 = undefined;
-
         // var ast_print = astPrint(ast, buffer[0..]);
-
         // print("\n ast_print : {s} \n", .{ast_print});
 
         _ = interpret(ast) catch {
             std.os.exit(70);
         };
     } else |_| {
-        print("\n Exit after error \n", .{});
-        std.os.exit(65);
+        // Don't exit the repl when a parse error is raised.
+
+        // print("\n Exit after error \n", .{});
+        // std.os.exit(65);
         return;
     }
 }
